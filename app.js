1dd6f7aa4a2b8270a9820cd47ba268832fcbd2de
时尽 <xingchen.sxc@alibaba-inc.com>
2016-06-05 18:16:06 +0800

    一点小改变

--- app.js
+++ app.js
@@ -95,58 +95,11 @@ router.get('/wechat',
 })
 );
 
-router.post('/wechat', 
-    wechat({
-        token: 'mytoken',
-        appid: 'wx0b4f6ee3da84307c',
-        appsecret: '2e6e21ead4da99a6f565b9fc07f138f5'
-    }).middleware(function *() {
-  // 微信输入信息都在this.weixin上
-  var message = this.weixin;
-  if (message.Content === 'diaosi') {
-    // 回复屌丝(普通回复)
-    this.body = 'hehe';
-  } else if (message.Content === 'text') {
-    //你也可以这样回复text类型的信息
-    this.body = {
-      content: 'text object',
-      type: 'text'
-    };
-  } else if (message.Content === 'hehe') {
-    // 回复一段音乐
-    this.body = {
-      type: "music",
-      content: {
-        title: "来段音乐吧",
-        description: "一无所有",
-        musicUrl: "http://mp3.com/xx.mp3",
-        hqMusicUrl: "http://mp3.com/xx.mp3"
-      }
-    };
-  } else if (message.Content === 'kf') {
-    // 转发到客服接口
-    this.body = {
-      type: "customerService",
-      kfAccount: "test1@test"
-    };
-  } else {
-    // 回复高富帅(图文回复)
-    this.body = [
-      {
-        title: '你来我家接我吧',
-        description: '这是女神与高富帅之间的对话',
-        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-        url: 'http://nodeapi.cloudfoundry.com/'
-      }
-    ];
-  }
-})
-);
 
 app
   .use(router.routes())
   .use(router.allowedMethods());
 
 app.listen(config.host.port);
-console.log(process.env.PORT)
+console.log(process.env)
 console.log('server is running on port'+config.host.port)

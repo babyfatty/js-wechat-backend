0af61faf14b2064f7c1b35d273868213cc779680
时尽 <xingchen.sxc@alibaba-inc.com>
2016-06-06 20:11:50 +0800

    ~

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var api = new API(config.wechat.appid, config.wechat.appsecret, function* () {
   // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
   yield fs.writeFile('access_token.txt', JSON.stringify(token));
 });
-
+wechat(config.wechat)
 // // 新增菜单
 
 // app.use(function*(next){
@@ -93,74 +93,8 @@ var api = new API(config.wechat.appid, config.wechat.appsecret, function* () {
 router.get('/register',function* (){
   this.body="hello"
 })
-router.get('/wechat',wechat(config.wechat).middleware(function *() {
-  // 微信输入信息都在this.weixin上
-  var message = this.weixin;
-  console.log(message)
-  if(message.Event === 'CLICK'){
-    switch(message.EventKey){
-      case 'V1001_GOOD':
-        this.body = {
-          content: '测试信息',
-          type:'text'
-        }
-        break;
-    }
-  }
-  else if (message.Event === 'unsubscribe'){
 
-  }
-  else if (message.Event === 'subscribe'){
-    this.body = [{
-                    title: '欢迎加入!!',
-                    description: ' 高富帅请输入: 1 \r\n 白富美请输入: 2 \r\n 屌丝请直接输入: diaosi',
-                    picurl: 'http://lxcdn.dl.files.xiaomi.net/mfsv2/download/s008/p01Z4fiL6J5k/QDICPcIYfmwoUL.jpg?thumb=320x320',
-                    url: 'http://weibo.com/fengjieluoyufeng?c=spr_qdhz_bd_baidusmt_weibo_s&nick=%E7%BD%97%E7%8E%89%E5%87%A4'
-                }]
-  }
-  else if (message.Content === 'diaosi') {
-    // 回复屌丝(普通回复)
-    this.body = 'hehe'+api.getIp();
-  } else if (message.Content === '1') {
-    //你也可以这样回复text类型的信息
-    this.body = {
-      content: 'text object',
-      type: 'text'
-    };
-  } else if (message.Content === '2') {
-    // 回复一段音乐
-    this.body = {
-      type: "music",
-      content: {
-        title: "来段音乐吧",
-        description: "一无所有",
-        musicUrl: "http://mp3.com/xx.mp3",
-        hqMusicUrl: "http://mp3.com/xx.mp3"
-      }
-    };
-  } else if (message.Content === '3') {
-    // 转发到客服接口
-    this.body = [
-      {
-        title: '你来我家接我吧',
-        description: '这是女神与高富帅之间的对话',
-        picurl: '//img.blog.csdn.net/20151030004840663',
-        url: '//baidu.com/'
-      }
-    ];
-  } else {
-    // 回复高富帅(图文回复)
-    this.body = [
-      {
-        title: '你来我家接我吧',
-        description: '这是女神与高富帅之间的对话',
-        picurl: '//img.blog.csdn.net/20151030004840663',
-        url: '//baidu.com/'
-      }
-    ];
-  }
-})
-)
+
 app
   .use(router.routes())
   .use(router.allowedMethods());

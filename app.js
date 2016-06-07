51be1d6dd8b9bb49a8c61c2ff6cc697cc9883718
时尽 <xingchen.sxc@alibaba-inc.com>
2016-06-07 09:32:24 +0800

    ~

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ router.get('/update',function* (){
   console.log('code',this.query.code)
   console.log('state',this.query.state)
   var option = {
-    url:"https://api.weixin.qq.com/sns/oauth2/access_token?appid="config.app.appid"&secret="+config.app.appsecret+"&code="+this.query.code+"&grant_type=authorization_code"
+    url:"https://api.weixin.qq.com/sns/oauth2/access_token?appid="+config.app.appid+"&secret="+config.app.appsecret+"&code="+this.query.code+"&grant_type=authorization_code"
   }
   var info = yield request(option)
   console.log(info.body)

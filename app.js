aa575f0a1b8141fd0ab7c85f58ae86f7d31cf0e2
时尽 <xingchen.sxc@alibaba-inc.com>
2016-06-07 09:07:27 +0800

    ~

--- app.js
+++ app.js
@@ -17,67 +17,67 @@ var api = new API(config.wechat.appid, config.wechat.appsecret, function* () {
 
 // // 新增菜单
 
-app.use(function*(next){
+// app.use(function*(next){
 
-  var menu = {
-   "button":[
-       {
-         "name":"考试报名",
-         "sub_button":[
-           {
-             "type":"view",
-             "name":"竞赛报名",
-             "url":"http://baidu.com"
-           },
-           {
-             "type":"click",
-             "name":"考场查询",
-             "key":"V1001_GOOD"
-           },{
-             "type":"click",
-             "name":"成绩查询",
-             "key":"V1001_GOOD"
-           }]
-        },
-       {
-         "name":"相关政策",
-         "sub_button":[
-           {
-             "type":"click",
-             "name":"考场查询",
-             "key":"V1001_GOOD"
-           },
-           {
-             "type":"click",
-             "name":"培训信息",
-             "key":"V1001_GOOD"
-           }]
-        },{
-         "name":"个人信息",
-         "sub_button":[
-           {
-             "type":"view",
-             "name":"基本信息绑定",
-             "url":"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx0b4f6ee3da84307c&redirect_uri=http%3A%2F%2F139.129.27.196%2Fregister&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect"
-           },
-           {
-             "type":"view",
-             "name":"个人信息完善",
-             "url":"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx0b4f6ee3da84307c&redirect_uri=http%3A%2F%2F139.129.27.196%2Fupdate&response_type=code&scope=snsapi_base&state=123#wechat_redirect"
-           },
-           {
-             "type":"click",
-             "name":"个人荣誉殿堂",
-             "key":"V1001_GOOD"
-           }]
-        }
-    ]
-  }
-  yield next
-  var result = yield* api.createMenu(menu);
-  console.log(result)
-  console.log('222')
-})
+//   var menu = {
+//    "button":[
+//        {
+//          "name":"考试报名",
+//          "sub_button":[
+//            {
+//              "type":"view",
+//              "name":"竞赛报名",
+//              "url":"http://baidu.com"
+//            },
+//            {
+//              "type":"click",
+//              "name":"考场查询",
+//              "key":"V1001_GOOD"
+//            },{
+//              "type":"click",
+//              "name":"成绩查询",
+//              "key":"V1001_GOOD"
+//            }]
+//         },
+//        {
+//          "name":"相关政策",
+//          "sub_button":[
+//            {
+//              "type":"click",
+//              "name":"考场查询",
+//              "key":"V1001_GOOD"
+//            },
+//            {
+//              "type":"click",
+//              "name":"培训信息",
+//              "key":"V1001_GOOD"
+//            }]
+//         },{
+//          "name":"个人信息",
+//          "sub_button":[
+//            {
+//              "type":"view",
+//              "name":"基本信息绑定",
+//              "url":"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx0b4f6ee3da84307c&redirect_uri=http%3A%2F%2F139.129.27.196%2Fregister&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect"
+//            },
+//            {
+//              "type":"view",
+//              "name":"个人信息完善",
+//              "url":"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx0b4f6ee3da84307c&redirect_uri=http%3A%2F%2F139.129.27.196%2Fupdate&response_type=code&scope=snsapi_base&state=123#wechat_redirect"
+//            },
+//            {
+//              "type":"click",
+//              "name":"个人荣誉殿堂",
+//              "key":"V1001_GOOD"
+//            }]
+//         }
+//     ]
+//   }
+//   yield next
+//   var result = yield* api.createMenu(menu);
+//   console.log(result)
+//   console.log('222')
+// })
 
 // // 查询菜单
 // app.use(function*(next){

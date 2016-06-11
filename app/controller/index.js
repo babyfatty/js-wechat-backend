eb0c2d859aba91a03b7bed300f97994181a16f5c
时尽 <xingchen.sxc@alibaba-inc.com>
2016-06-11 22:19:03 +0800

    ~

--- app/controller/index.js
+++ app/controller/index.js
@@ -3,7 +3,7 @@ var update = require('./update')
 var wechat = require('./wechat')
 
 exports.admin=require('./admin')
-exports.register=register,
+exports.register=register
 exports.update= update
 exports.wechat=wechat
 exports.userRegister = require('./userRegister')

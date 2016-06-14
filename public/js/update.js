4a6875c22ab473ee9fa7211a96b3be8f867e531d
时尽 <xingchen.sxc@alibaba-inc.com>
2016-06-14 11:14:19 +0800

    ~

--- public/js/update.js
+++ public/js/update.js
@@ -117,17 +117,14 @@ $('#submitBtn').on('click',function(){
 	var s = {}
 	$.post('http://aosaikang.xiaonian.me/api/student/update', { 
 	 "s.id":parseInt(id),
-	 "s.name":param[0].value,
-	 "s.gender":param[1].value,
-	 "s.grade":param[4].value,
-	 "s.class":param[8].value,
-	 "s.cz_school":param[7].value,
-	 "s.cz_type":param[6].value,
-	 "s.gz_school":param[5].value,
-	 "s.city": param[3].value,
-	 "s.birthday": param[2].value,
-	 "s.parent_name": param[9].value,
-	 "s.parent_phone": param[10].value
+	 "s.grade":param[1].value,
+	 "s.class":param[5].value,
+	 "s.cz_school":param[4].value,
+	 "s.cz_type":param[3].value,
+	 "s.gz_school":param[2].value,
+	 "s.city": param[0].value,
+	 "s.parent_name": param[6].value,
+	 "s.parent_phone": param[7].value
 	}, function(response,err){
   		window.location.replace("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxab5e05ece55fcade&redirect_uri=http%3A%2F%2Faosaikangjs.xiaonian.me%2Fsuccess&response_type=code&scope=snsapi_base&state=123#wechat_redirect")
 	})

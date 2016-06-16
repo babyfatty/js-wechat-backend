9bbbd1af0d59a65fa0a7a064870eae4054d88138
时尽 <xingchen.sxc@alibaba-inc.com>
2016-06-16 16:40:39 +0800

    ~

--- public/js/revise.js
+++ public/js/revise.js
@@ -158,7 +158,7 @@ $('#updateForm').validator({
 			  	}
 			})
 		}else{
-			if((!isChecked || !getCheck())){
+			if((!isChecked && !getCheck())){
 				$('.successres').hide()
 				$('.sendFailres').hide()
 				$('.telFailres').hide()
